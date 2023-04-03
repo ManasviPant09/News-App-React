b56b2b1d5960c9529a578ccf172fdcafb4d46a51
Manasvi Pant <88909122+ManasviPant09@users.noreply.github.com>
2023-04-03 17:21:35 +0530

    Update Signup.jsx

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -135,7 +135,8 @@ const Signup = () => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorCode,errorMessage)
+        console.log(errorCode,errorMessage);
+        alert(errorMessage);
       });
   }; 
   return (
